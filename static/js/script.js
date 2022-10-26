24836c16b3f2c61fc5d9d246372a10ff52ef3fe5
Seungwoo Ryu <ryuryu10@naver.com>
2022-10-26 14:07:27 +0900

    모달 기능 추가 완료

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,8 @@ var INDEX_NowScrollValue = 1;
 var INDEX_MaxScrollValue = 0;
 var INDEX_ScrollLeftButton = document.querySelector(".LeftActionButton");
 var INDEX_ScrollRightButton = document.querySelector(".RightActionButton");
+var NAV_ModalCloseButton = document.querySelector("#NAV_Close");
+var NAV_WantReadBookButton = document.querySelectorAll(".bottom > .nav-container >.nav-items")[2];
 for(i=0;i<=99;i++){
     INDEX_MaxScrollValue++;
     if(!document.querySelector("#B"+INDEX_MaxScrollValue)){
@@ -32,4 +34,10 @@ INDEX_ScrollLeftButton.addEventListener('click',function() {
         $('#B'+INDEX_NowScrollValue).show();
     }
     console.log(INDEX_MaxScrollValue, INDEX_NowScrollValue);
+})
+NAV_ModalCloseButton.addEventListener('click',function() {
+    document.querySelector(".nav_overlay").style.display = "none";
+})
+NAV_WantReadBookButton.addEventListener('click',function() {
+    document.querySelector(".nav_overlay").style.display = "flex";
 })
